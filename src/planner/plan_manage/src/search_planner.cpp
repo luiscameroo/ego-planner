f59f7b3ba634b2e267791ad6bf5df1e34053787c
Luis Camero <luis.camero@mail.utoronto.ca>
2024-03-31 14:41:53 -0400

    Added code to get current position of drone

--- src/planner/plan_manage/src/search_planner.cpp
+++ src/planner/plan_manage/src/search_planner.cpp
@@ -1,4 +1,7 @@
 #include <ros/ros.h>
+#include <tf2_ros/transform_listener.h>
+#include <geometry_msgs/TransformStamped.h>
+#include <geometry_msgs/Twist.h>
 #include <geometry_msgs/PoseStamped.h>
 #include <std_msgs/String.h>
 #include <nav_msgs/Odometry.h>
@@ -19,24 +22,26 @@ int main(int argc, char **argv)
   ros::init(argc, argv, "search_node");
   ros::NodeHandle nh("~");
 
+  // Publisher to send goal to ego_planner
   ros::Publisher goal_pub = nh.advertise<geometry_msgs::PoseStamped>("/move_base_simple/goal", 10);
 
+  // Subscriber to read odometry messages
   ros::Subscriber odom_sub = nh.subscribe("/visual_slam/odom", 1000, OdomCallback);
 
-  ros::Rate loop_rate(10);
-  ros::Rate wait_rate(0.5);
+  // Subsciber to Listen to transform
+  tf2_ros::Buffer tf_buffer;
+  tf2_ros::TransformListener tf_listener(tf_buffer);
 
-  /**
-  * A count of how many messages we have sent. This is used to create
-  * a unique string for each message.
-  */
+  // List of goal pose
+  std::vector<geometry_msgs::PoseStamped> goals; 
+
+
+  // Add poses that go to each of the four corners
   float min_x = -15;
   float max_x = 15;
   float min_y = -15;
   float max_y = 15;
 
-  std::vector<geometry_msgs::PoseStamped> goals; 
-  
   geometry_msgs::PoseStamped goal;
   goal.pose.position.x = min_x;
   goal.pose.position.y = min_y;
@@ -63,27 +68,49 @@ int main(int argc, char **argv)
   goal.pose.position.z = 1.0;
   goals.push_back(goal);
 
+  // Rates to define how often to check to go next step
+  ros::Rate loop_rate(10);
+  ros::Rate wait_rate(0.1);
+
+  // Main Loop
   int count = 0;
   while (ros::ok())
   {
-    ros::spinOnce();
-    loop_rate.sleep();
+    // Get the current pose of the drone
+    geometry_msgs::TransformStamped tf;
+    try
+    {
+      tf = tf_buffer.lookupTransform("world", "base", ros::Time(0));
+      ROS_INFO("Position: %.3f, %.3f, %.3f",
+        tf.transform.translation.x,
+        tf.transform.translation.y,
+        tf.transform.translation.z
+      );
+    }
+    catch (tf2::TransformException &ex)
+    {
+      ROS_WARN("%s", ex.what());
+      ros::Duration(1.0).sleep();
+      continue; 
+    }
 
     if(SPEED <= 0.00001)
     {
       if(count >= int(goals.size())) break;
-      ROS_INFO("Stopped. Speed: %.2f. Move to next point.", SPEED);
+      ROS_INFO("Stopped. Speed: %.3f. Move to next point.", SPEED);
       // Publish Goal Pose
       goal_pub.publish(goals[count]);
       ++count;
+      // Wait for motion to commence
       wait_rate.sleep();
     }
     else
     {
-      ROS_INFO("Moving at speed %.2f...", SPEED);
+      ROS_INFO("Moving at speed %.3f...", SPEED);
     }
 
-
+    ros::spinOnce();
+    loop_rate.sleep();
   }
   return 0;
 }
